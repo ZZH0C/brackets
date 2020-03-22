bec934f019fd525687e571c4a4d29737b7ec0c86
zzh0c <zzh0czz@gmail.com>
2020-03-22 13:08:58 +0300

    init: task solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let brack=[];
+    for (let i=0;i<bracketsConfig.length;i++){
+        while (true){
+            brack=bracketsConfig[i][0]+bracketsConfig[i][1];
+            if(str.includes(brack))
+            {
+                str=str.replace(brack,"");
+                i=0;
+            }
+            else {
+            break;}
+        }
+    }
+    return str.length===0;
+};
